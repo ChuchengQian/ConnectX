fedf8066ce87071f39d036130ec7ba308342fc71
Chucheng Qian <chuchengqian@ChuchengmatoMacBook-Pro.local>
2017-10-22 02:52:20 +1100

    ass03,remove red7

--- src/Bot/Blue.hs
+++ src/Bot/Blue.hs
@@ -28,7 +28,7 @@ hehe   p = case p of
 
 
 findindex :: Board -> LookAhead -> Int ->(Alpha,Beta) -> [Pair]
-findindex b i a (alpha,beta) = zip (map (scorelist i a (alpha,beta)) (validboardlist b)) [1..11]
+findindex b i a (alpha,beta) = zip (map (scoreList i (turn b) a (alpha,beta)) (nextboards b)) [1..11]
 
 
 
@@ -44,40 +44,56 @@ heuristic    b p = case p of
 
 
 
+scoreList :: LookAhead -> Player -> Int -> (Alpha,Beta) -> Board -> Score
+scoreList   i mp a (alpha,beta) b = case (a==i || (nextboards b) == []) of
+            True        -> heuristic b mp
+            False
+                | mp == (turn b)  -> maximise (nextboards b) (alpha, beta)
+                | otherwise       -> minimise (nextboards b) (alpha, beta)
 
-scorelist :: LookAhead -> Int ->(Alpha,Beta) -> Board -> Score
-scorelist   i a (alpha,beta) b = case (a==i) of
-            True        -> heuristic b (turn b)
-            False       -> case (turn b) of
-                                      BlueBot -> maximise (validboardlist b) (alpha,beta)
-                                      RedBot  -> minimise (validboardlist b) (alpha,beta)
-                                      Finished -> 0
         where
             maximise:: [Board] -> (Alpha,Beta)-> Score
             maximise boards (alpha1,beta1) = case boards of
                      [] -> alpha1
                      x:xs
-                         |  alpha1 >= beta1     -> alpha1
-                         |  (scorelist i (a+1) (alpha1,beta1) x) < alpha1  -> maximise xs  (alpha1,beta1)
-                         |  (scorelist i (a+1) (alpha1,beta1) x) >=alpha1 && (scorelist i (a+1) (alpha1,beta1) x) <= beta1 -> maximise xs  ((heuristic x (turn x)),beta1)
+                         |  alpha1 >= beta1     -> beta1
+                         |  (scoreList i mp (a+1) (alpha1,beta1) x) >= beta1 -> beta1
+                         |  (scoreList i mp (a+1) (alpha1,beta1) x) > alpha1 -> maximise xs ((scoreList i mp (a+1) (alpha1,beta1) x),beta1)
+                         |  otherwise -> maximise xs (alpha1,beta1)
+                        -- |  (scoreList i mp (a+1) (alpha1,beta1) x) < alpha1  -> maximise xs  (alpha1,beta1)
+                        --  |  (scoreList i mp (a+1) (alpha1,beta1) x) >=alpha1 && (scoreList i (a+1) (alpha1,beta1) x) <= beta1 -> maximise xs  ((heuristic x (turn x)),beta1)
 
 
             minimise:: [Board] -> (Alpha,Beta)-> Score
             minimise boards (alpha2,beta2) = case boards of
                      [] -> beta2
                      x:xs
-                         |  alpha2 >= beta2    -> beta2
-                         |  (scorelist i (a+1) (alpha2,beta2) x) < beta2  -> minimise xs  ((heuristic x (turn x)),beta2)
-                         |  (scorelist i (a+1) (alpha2,beta2) x) >= beta2 && (scorelist i (a+1) (alpha2,beta2) x) >= alpha2-> minimise xs  (alpha2,beta2)
+                         |  alpha2 >= beta2    -> alpha2
+                         | (scoreList i mp (a+1) (alpha2,beta2) x) <= alpha2 -> alpha2
+                         | (scoreList i mp (a+1) (alpha2,beta2) x) <= beta2 -> minimise xs (alpha2,(scoreList i mp (a+1) (alpha2,beta2) x))
+                         | otherwise -> minimise xs (alpha2,beta2)
+                        -- |  (scoreList i (a+1) (alpha2,beta2) x) < beta2  -> minimise xs  ((heuristic x (turn x)),beta2)
+                       --  |  (scoreList i (a+1) (alpha2,beta2) x) >= beta2 && (scoreList i (a+1) (alpha2,beta2) x) >= alpha2-> minimise xs  (alpha2,beta2)
+
+nextboards :: Board -> [Board]
+nextboards b = map (updateBoard b) (filter (validMove b) [1..width])
+    where
+        width = getWidth b
+
+getWidth :: Board -> Int
+getWidth    b = fst $ dimension b
 
 
-validboardlist:: Board -> [Board]
-validboardlist b = map (updateBoard b) (validmovelist b 1)
 
-validmovelist:: Board -> Index -> [Index]
-validmovelist   b i = case (i <= 11) of
-            True -> case (validMove b i) of
-                                True  -> [i]++(validmovelist b (i+1))
-                                False -> []++ (validmovelist b (i+1))
-            False -> []
 
+
+
+--validboardlist:: Board -> [Board]
+--validboardlist b = map (updateBoard b) (validmovelist b 1)
+
+--validmovelist:: Board -> Index -> [Index]
+--validmovelist   b i = case (i <= 11) of
+ --           True -> case (validMove b i) of
+  --                              True  -> [i]++(validmovelist b (i+1))
+ --                               False -> []++ (validmovelist b (i+1))
+ --
